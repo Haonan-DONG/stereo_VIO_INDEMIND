7d25f3f4cbd4295d3eeb61ff7dd70563628b2135
Haonan-Dong <august_dong@163.com>
2024-09-08 21:45:45 +0800

    [feat] add indemind sdk

---
+++ IMSEE-SDK/ros/devel/share/imsee_ros_wrapper/cmake/imsee_ros_wrapper-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(imsee_ros_wrapper_MESSAGE_FILES "")
+set(imsee_ros_wrapper_SERVICE_FILES "")
---
+++ IMSEE-SDK/ros/devel/share/imsee_ros_wrapper/cmake/imsee_ros_wrapper-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(imsee_ros_wrapper_MSG_INCLUDE_DIRS "")
+set(imsee_ros_wrapper_MSG_DEPENDENCIES std_msgs)
